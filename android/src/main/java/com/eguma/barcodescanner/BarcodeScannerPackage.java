14ccd36bd069cae9e75791d26819f27a3fa03fa3
Bruno Barbieri <brunobar79@gmail.com>
2020-01-23 00:38:19 -0500

    Update BarcodeScannerPackage.java

--- android/src/main/java/com/eguma/barcodescanner/BarcodeScannerPackage.java
+++ android/src/main/java/com/eguma/barcodescanner/BarcodeScannerPackage.java
@@ -1,32 +1,30 @@
 package com.eguma.barcodescanner;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class BarcodeScannerPackage implements ReactPackage {
+
     @Override
-    public List<NativeModule> createNativeModules(
-            ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
         return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
     @Override
-    public List<ViewManager> createViewManagers(
-            ReactApplicationContext reactContext) {
-        return Arrays.<ViewManager>asList(
-                new BarcodeScannerManager()
-        );
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        return Collections.<ViewManager>singletonList(new BarcodeScannerManager());
     }
+
 }
